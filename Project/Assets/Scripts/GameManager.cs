a644bce8797d9d78f8167a582ebe197eda1eb9e2
marivsteo <mariusteo99@gmail.com>
2020-03-11 12:11:17 +0200

    Uploaded the game

---
+++ Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+
+    public float levelStartDelay = 2f;
+    public float turnDelay = .1f;
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+
+    private Text levelText;
+    private GameObject levelImage;
+    private int level = 0;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private bool doingSetup;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        instance.level++;
+        //Call InitGame to initialize our level.
+        InitGame();
+    }
+
+    void OnEnable()
+    {
+        //Tell our ‘OnLevelFinishedLoading’ function to start listening for a scene change event as soon as this script is enabled.
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        //Tell our ‘OnLevelFinishedLoading’ function to stop listening for a scene change event as soon as this script is disabled.
+        //Remember to always have an unsubscription for every delegate you subscribe to!
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void InitGame()
+    {
+        doingSetup = true;
+        
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        if (level == 1)
+        {
+            levelText.text = "Le gouvernement a envoyé Olivier sur une île isolée pour lui éduquer sur l'importance d'une alimentation saine. Son but est de survivre autant de jours que possible avec les quantités de nourriture qu'il accueille, mais il y a des consequences de la part des malsains choix. (Attention: les hamburgers sont mauvais, les légumes sont bons, les monstres vous attaquent, aller au panneau de sortie)  " +
+                  "\n \n Jour " + level;
+            levelStartDelay = 20f;
+        }
+        else
+        {
+            levelText.text = "Jour " + level;
+            levelStartDelay = 2f;
+        }
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+	public void GameOver()
+    {
+        levelText.text = "Après " + level + " jours, il est mort de faim.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (playersTurn || enemiesMoving || doingSetup)
+            return;
+        StartCoroutine(MoveEnemies());
+	}
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count ==0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i=0; i< enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+
+
+}
+
